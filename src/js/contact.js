83fa2e28f6c26efe678e2fc6a738fa774903091a
ilhamtrim <ilhamtrim.id@gmail.com>
2023-10-18 12:08:04 +0700

    update active link ketika scroll

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,10 +1,27 @@
+// <!-- ============== Contact Form to Spreadsheet ================ -->
 
-  const scriptURL = '<SCRIPT URL>'
-  const form = document.forms['submit-to-google-sheet']
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-  })
+    const scriptURL = 'https://script.google.com/macros/s/AKfycby_kgFevvLdNlsJF0ANnbJLwWlmnbDWrCy40XuMnG200mXzzDL_N0-YfjyclUtdG2e5/exec'
+    const form = document.forms['iproject-contact-form']
+    const btnKirim = document.querySelector('.btn-kirim');
+    const btnLoading = document.querySelector('.btn-loading');
+    const myAlert = document.querySelector('.my-alert')
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+    //   Ketika tombol submit diclick
+    // tampilkan tombol loading , tampilkan tombol kirim
+    btnLoading.classList.toggle('d-none');
+    btnKirim.classList.toggle('d-none');
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            // tampilkan tombol kirim , hilangkan tombol loading
+            btnLoading.classList.toggle('d-none');
+            btnKirim.classList.toggle('d-none');
+            // Tampilkan alert
+            myAlert.classList.toggle('d-none')
+            // Reset Form
+            form.reset();
+            console.log('Success!', response)
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
